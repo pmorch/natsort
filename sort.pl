01252428862d3eac9415d4ff6c2e8be6a0924ab3
Peter V. Mørch <peter@morch.com>
2011-07-04 10:18:53 +0200

    A wrapper around alphanum.pl that measures execution time

---
+++ sort.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+use strict;
+use Time::HiRes qw(time);
+
+do "alphanum.pl";
+
+scalar @ARGV == 1
+    or die "Usage: $0 <number of array elements>";
+
+my $numElements = $ARGV[0];
+my @array = map { 'a' . int(rand($numElements)) } (1..$numElements);
+
+my $numCmp = 0;
+sub sortSub {
+    $numCmp++;
+    return alphanum($a, $b);
+    return $a cmp $b;
+}
+
+my $begin = time;
+my @b = sort sortSub @array;
+my $end = time;
+printf "%d elements, %d comparisons took %fs\n",
+    $numElements, $numCmp, ($end-$begin);
